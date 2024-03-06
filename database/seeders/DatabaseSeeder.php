6c90effe891e8c3193610a61bae06aab7f022b4f
Carlos Iturralde <iturraldec@gmail.com>
2024-03-06 14:00:48 -0400

    Creacion de usuarios administrador y de pruebas. [master]

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,7 +3,14 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\User;
 use Illuminate\Database\Seeder;
+
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\Models\Permission;
+use Illuminate\Support\Facades\Hash;
+
 
 class DatabaseSeeder extends Seeder
 {
@@ -18,5 +25,31 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+
+        // Reset cached roles and permissions
+        app()[\Spatie\Permission\PermissionRegistrar::class]->forgetCachedPermissions();
+
+        // create permissions
+        Permission::create(['name' => 'users.index']);
+        Permission::create(['name' => 'users.create']);
+        Permission::create(['name' => 'users.update']);
+        Permission::create(['name' => 'users.delete']);
+
+        // create roles and assign created permissions
+        $role = Role::create(['name' => 'Administrador de Usuarios']);
+        $role->givePermissionTo(Permission::all());
+
+        // creo el administrador de usuarios
+        $user = User::create([
+            'name' => 'Jean Pier',
+            'email' => 'jeanpier@gmail.com',
+            'password' => Hash::make('12345678')
+        ]);
+
+        // le asigno el rol de administrador de usuarios
+        $user->assignRole('Administrador de Usuarios');
+
+        // usuarios de prueba
+        User::factory(10)->create();
     }
 }
