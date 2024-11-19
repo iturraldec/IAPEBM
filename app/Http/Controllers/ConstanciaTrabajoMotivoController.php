828729388cc41bffcdfbed09ccb7c125cb68ce19
Carlos Iturralde <iturraldec@gmail.com>
2024-11-19 15:44:52 -0400

    Validacion de motivos de C.T. [PoliceRefactory]

--- app/Http/Controllers/ConstanciaTrabajoMotivoController.php
+++ app/Http/Controllers/ConstanciaTrabajoMotivoController.php
@@ -19,55 +19,63 @@ class ConstanciaTrabajoMotivoController extends Controller
     $this->_requestResponse = $requestResponse;
   }
 
-    /**
-     * Display a listing of the resource.
-     */
-    public function index()
-    {
-      return request()->ajax() ? datatables()->of(ConstanciaTrabajoMotivo::orderBy('motivo'))->toJson()
-                             : view('ct-motivos.index');
-    }
+  /**
+   * Display a listing of the resource.
+   */
+  public function index()
+  {
+    return request()->ajax() ? datatables()->of(ConstanciaTrabajoMotivo::orderBy('motivo'))->toJson()
+                            : view('ct-motivos.index');
+  }
 
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request)
-    {
-      $motivo = ConstanciaTrabajoMotivo::create(['motivo' => $request->motivo]);
-      
-      //
-      $this->_requestResponse->success = true;
-      $this->_requestResponse->message = 'Motivo de la constancia de trabajo agregado!';
-      $this->_requestResponse->data    = $motivo;
+  /**
+   * Store a newly created resource in storage.
+   */
+  public function store(Request $request)
+  {
+    $request->validate([
+      'motivo' => 'required|max:100|unique:constancia_trabajo_motivos',
+    ]);
 
-      return response()->json($this->_requestResponse, Response::HTTP_CREATED);
-    }
+    $motivo = ConstanciaTrabajoMotivo::create(['motivo' => $request->motivo]);
+    
+    //
+    $this->_requestResponse->success = true;
+    $this->_requestResponse->message = 'Motivo de la constancia de trabajo agregado!';
+    $this->_requestResponse->data    = $motivo;
 
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, ConstanciaTrabajoMotivo $ct_motivo)
-    {
-      $ct_motivo->update(['motivo' => $request->motivo]);
-      
-      //
-      $this->_requestResponse->success = true;
-      $this->_requestResponse->message = 'Motivo de la constancia de trabajo modificado!';
+    return response()->json($this->_requestResponse, Response::HTTP_CREATED);
+  }
 
-      return response()->json($this->_requestResponse, Response::HTTP_OK);
-    }
+  /**
+   * Update the specified resource in storage.
+   */
+  public function update(Request $request, ConstanciaTrabajoMotivo $ct_motivo)
+  {
+    $request->validate([
+      'motivo' => 'required|max:100|unique:constancia_trabajo_motivos',
+    ]);
 
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(ConstanciaTrabajoMotivo $ct_motivo)
-    {
-      $ct_motivo->delete();
+    $ct_motivo->update(['motivo' => $request->motivo]);
+    
+    //
+    $this->_requestResponse->success = true;
+    $this->_requestResponse->message = 'Motivo de la constancia de trabajo modificado!';
 
-      //
-      $this->_requestResponse->success = true;
-      $this->_requestResponse->message = 'Motivo de Constancia de Trabajo eliminado!';
+    return response()->json($this->_requestResponse, Response::HTTP_OK);
+  }
 
-      return response()->json($this->_requestResponse, Response::HTTP_OK);
-    }
+  /**
+   * Remove the specified resource from storage.
+   */
+  public function destroy(ConstanciaTrabajoMotivo $ct_motivo)
+  {
+    $ct_motivo->delete();
+
+    //
+    $this->_requestResponse->success = true;
+    $this->_requestResponse->message = 'Motivo de Constancia de Trabajo eliminado!';
+
+    return response()->json($this->_requestResponse, Response::HTTP_OK);
+  }
 }
